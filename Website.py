040faf411dd56b825cc645b938eae355fcf1f6b6
Lukas Keilholz <lukas.keilholz@web.de>
2017-01-12 18:55:29 +0100

    Initial commit

---
+++ Website.py
@@ -0,0 +1,13 @@
+import sys
+
+
+class Website:
+
+    def __init__(self, isUsingAjax, isMultiPage):
+        self.isUsingAjax = isUsingAjax
+        self.isMultiPage = isMultiPage
+        self.nextPageHtmlFrag
+        self.listItemHtmlFrag
+
+
+
